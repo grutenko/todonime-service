9573694a4b3f79fdd096240aba870873bda1c65f
Alexey Fedorenko <alexfedor932@gmail.com>
2020-05-04 04:25:44 +0300

    Консольная команда для получения постеров.

---
+++ src/Console/Anime/Poster/Get.php
@@ -0,0 +1,101 @@
+<?php
+
+
+namespace App\Console\Anime\Poster;
+
+
+use Grutenko\Shikimori\Sdk;
+use League\Flysystem\Filesystem;
+use MongoDB\Database;
+use Psr\Container\ContainerInterface;
+use RuntimeException;
+use \Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Get extends Command
+{
+    /**
+     * @var string
+     */
+    protected static $defaultName = 'anime:poster.get';
+
+    /**
+     * @var Database
+     */
+    private $db;
+
+    /**
+     * @var Sdk
+     */
+    private $sdk;
+
+    /**
+     * @var Filesystem
+     */
+    private $storage;
+
+    /**
+     * GetAnimes constructor.
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->db = $container->get('mongodb')->todonime;
+        $this->sdk = $container->get('shikimori_sdk');
+        $this->storage = $container->get('storage');
+
+        parent::__construct();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function configure()
+    {
+        $this
+            ->setDescription('Получает постеры для аниме и сохраняет в ханилище.');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $animes = $this->db->animes
+            ->find()
+            ->toArray();
+
+        $count = 0;
+        $ids = array_column($animes, 'shikimori_id');
+
+        $ids = array_filter($ids, function($id) {
+            return !$this->storage->has("/anime/{$id}/poster_original");
+        });
+
+        $output->writeln("Сохраняю постеры...");
+
+        foreach($ids as $id) {
+            $anime = $this->sdk->anime()->findOrFail($id);
+
+            $output->write(
+                "\r". ($count++) . '/' . count($ids) . ' '
+                . str_pad(
+                    substr( $anime->name, 0, 50),
+                    50,
+                    ' ',
+                    STR_PAD_LEFT
+                )
+            );
+
+            $success = $this->storage->writeStream(
+                "/anime/{$id}/poster_original",
+                $anime->getPoster()->detach()
+            );
+
+            if(!$success) {
+                throw new RuntimeException('Error writing poster');
+            }
+        }
+    }
+}
