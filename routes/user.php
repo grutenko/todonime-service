78e3b55783e932b286be78fa41e2e651d10a80e8
grutenko <alex@grutenko.ru>
2020-05-20 04:08:23 +0300

    HotFix: GET /user/logout POST /user/logout

--- routes/user.php
+++ routes/user.php
@@ -8,4 +8,4 @@ $group->put('/user/episode/watched', \App\Action\User\BumpEpisode::class);
  */
 $group->get('/user/current', \App\Action\User\Current::class);
 
-$group->get('/user/logout', \App\Action\User\Logout::class);
+$group->post('/user/logout', \App\Action\User\Logout::class);
