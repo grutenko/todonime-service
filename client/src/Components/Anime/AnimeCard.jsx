924f1b6d8195a250902d47fc85a3474c9becc5d8
grutenko <alex@grutenko.ru>
2020-06-11 23:41:33 +0300

    Сменил дизайн карточки аниме

--- client/src/Components/Anime/AnimeCard.jsx
+++ client/src/Components/Anime/AnimeCard.jsx
@@ -4,6 +4,7 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Button from "@material-ui/core/Button";
+import {Star, StarBorder, StarHalf} from "@material-ui/icons";
 
 export default function AnimeCard ({anime, currentEpisode, lastEpisode, user}) {
 
@@ -19,6 +20,14 @@ export default function AnimeCard ({anime, currentEpisode, lastEpisode, user}) {
         },
         text: {
             margin: "10px 15px 15px 15px",
+        },
+        progress: {
+            margin: '0'
+        },
+        progressText: {
+            fontSize: '12px',
+            color: '#898989',
+            margin: '0 15px'
         }
     };
 
@@ -30,24 +39,31 @@ export default function AnimeCard ({anime, currentEpisode, lastEpisode, user}) {
         />
         <div style={styles.content}>
             <div style={styles.text}>
-                <Typography component="h5" variant="h5">
-                    {anime.name_ru || anime.name_en}
-                </Typography>
-                <Typography variant="subtitle1" color="textSecondary">
-                    {lastEpisode} / {anime.last_episode}
-                </Typography>
-                <Button
-                    variant ="outlined"
-                    color   ="primary"
-                    href    ={"https://shikimori.one" + anime.url}
-                >
-                    Подробнее на shikimori
-                </Button>
+                <a href = {"https://shikimori.one" + anime.url}>
+                    <Typography component="h5" variant="h5">
+                        {(anime.name_ru  ? anime.name_ru + ' / ': '') + anime.name_en}
+                    </Typography>
+                </a>
+                <Rating rating={anime.rating} />
             </div>
+            <span style={styles.progressText}>Текущий прогресс: {lastEpisode} / {anime.last_episode}</span>
             <LinearProgress
+                style={styles.progress}
                 variant="determinate"
                 value={(lastEpisode / anime.last_episode) * 100}
             />
         </div>
     </Card>
 }
+
+const Rating = ({rating}) =>
+    <div style={{display: 'flex'}}>
+        {Array.from(Array(5).keys()).map((i) =>
+            i+1 <= Math.floor(rating/2)
+                ? <Star color="primary"/>
+                : rating/2 - Math.floor(rating/2) < 0.25
+                    ? <StarBorder color="primary"/>
+                    : <StarHalf color="primary" />
+        )}
+        <span style={{margin: 'auto 15px'}}>{rating}</span>
+    </div>
