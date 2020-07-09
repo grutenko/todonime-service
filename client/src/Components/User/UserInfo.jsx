e5fbfb0f59c61c423cc1025cb18c70dfacacb701
grutenko <alex@grutenko.ru>
2020-07-10 00:25:11 +0300

    Доработал меню пользоателя вывел основновные данные и список последних обновлений просмотренного

--- client/src/Components/User/UserInfo.jsx
+++ client/src/Components/User/UserInfo.jsx
@@ -4,64 +4,51 @@ import {fetch} from "../../lib/api";
 import { setShow } from "../../lib/promt";
 
 import './UserInfo.css';
+import Avatar from "@material-ui/core/Avatar";
+import Chip from "@material-ui/core/Chip";
 
-export default function UserInfo () {
+import WatchList from '../Anime/WatchList';
 
-    const [
-            user,
-            setUser
-        ] = React.useState(null),
-        [
-            load,
-            setLoad
-        ] = React.useState(false),
-
-        logout = () => {
-            setShow('logout');
-            fetch('user/logout', {}, 'POST').then(data => {
-                window.location.href = window.location;
-            });
-        },
-        login = () => {
-            setShow('login');
-            window.location.href = `${process.env.REACT_APP_AUTH_BASE}?back_url=${window.location}`;
-        }
-
-    if (!load) {
-
-        fetch("user/current").then((data) => {
-
-            setUser(!data.error ? data.data.user : null);
-            setLoad(true);
-
-        })
-            .catch((data) => {
-
-                setUser(null); setLoad(true);
-
-            });
-
+export default class UserInfo extends React.Component {
+    logout() {
+        setShow('logout');
+        fetch('user/logout', {}, 'POST').then(data => {
+            window.location.href = window.location;
+        });
     }
 
-    return <div className="user-info__root">
-        {load
-            ? user == null
-                ? <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={login}
-                >
-                    Авторизация через shikimori.one
-                </Button>
-                : <Button
-                    variant = "contained"
-                    color   = "primary"
-                    onClick = {logout}
-                >
-                    Выйти
-                </Button>
-            : null
-        }
-    </div>;
+    login() {
+        setShow('login');
+        window.location.href = `${process.env.REACT_APP_AUTH_BASE}?back_url=${window.location}`;
+    }
 
+    renderUserData() {
+        const {user} = this.props;
+
+        return <div className="user-info__header">
+            <Avatar alt={user.nickname} src={user.avatar}/>
+            <span className="user-info__name">
+                {user.nickname}
+            </span>
+            <Scopes scopes={user.scope}/>
+        </div>
+    }
+
+    render() {
+        return <div>
+            { this.renderUserData() }
+            <WatchList />
+        </div>
+    }
+}
+
+
+function Scopes({scopes}) {
+    return <>
+        {scopes.map(scope => <Chip
+            className="comment__chip"
+            color="primary"
+            label={scope}
+        />)}
+    </>
 }
