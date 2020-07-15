fa965dac1be379ecf78c26baa17ba08d4f71159c
grutenko <alex@grutenko.ru>
2020-07-15 09:15:51 +0300

    Убрал задержку

--- config/middleware.php
+++ config/middleware.php
@@ -1,11 +1,4 @@
 <?php
 return [
-    new \App\Middleware\AuthMiddleware($container),
-    function($request, $handler) {
-        $response = $handler->handle($request);
-
-        usleep(100000 - (microtime() - __START__));
-
-        return $response;
-    }
+    new \App\Middleware\AuthMiddleware($container)
 ];
